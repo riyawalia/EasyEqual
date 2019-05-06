300012be68389545896373034339cf562016b5a9
riyawalia <riyawalia12@gmail.com>
2019-05-06 14:01:43 -0400

    fixed weird merge of soln

---
+++ EasyEqual/EasyEqual/EasyEqual.Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace EasyEqual.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
